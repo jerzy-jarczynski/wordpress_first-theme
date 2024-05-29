f63b53d1040a5488726f1ef812fd3f24fe0b21b8
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2024-05-29 23:22:16 +0200

    Add main.css, Add Top Menu and Footer Menu, basic styles

--- header.php
+++ header.php
@@ -9,3 +9,18 @@
 
 </head>
 <body>
+
+<header>
+
+  <div class="container">
+    <?php
+      wp_nav_menu(
+        array(
+          'theme_location' => 'top-menu',
+          'menu_class' => 'top-bar',
+        )
+      );
+    ?>
+  </div>
+
+</header>
