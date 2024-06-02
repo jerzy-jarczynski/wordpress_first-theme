c5f72603f68e61cdd329d38111acced01bffbc7e
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2024-06-02 05:35:03 +0200

    Add blog category, archive, blogcontent: date, content, author, tags, categories, comments; add thumbnails, pagination

--- page.php
+++ page.php
@@ -5,6 +5,10 @@
 
     <h1><?php the_title();?></h1>
 
+    <?php if(has_post_thumbnail()):?>
+      <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title();?>" class="img-fluid mb-3 img-thumbnail">
+    <?php endif;?>
+
     <?php get_template_part('includes/section', 'content');?>
 
   </div>
